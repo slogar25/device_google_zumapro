327f5ca1782a549ff6ab30d584866636d87309a1
Alessandro Astone <ales.astone@gmail.com>
2021-01-30 00:55:01 +0100

    Build pixel touch HAL
    Change-Id: I45cff81e1159613094440dd7b2b73b53519c9de6

--- BoardConfig-calyx.mk
+++ BoardConfig-calyx.mk
@@ -3,3 +3,28 @@
 # SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/calyx/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += \
+    device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
+    device/google/zumapro/manifest_radio_ds.xml
+
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor \
+    vendor_dlkm
+
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# Enable chain partition for vendor.
+BOARD_AVB_VBMETA_VENDOR := vendor
+BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
+
+AB_OTA_PARTITIONS += \
+    vbmeta_vendor
