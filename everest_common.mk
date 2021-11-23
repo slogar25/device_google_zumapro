8f4c6ed0226b0afceaa844962c0009488a23425a
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- everest_common.mk
+++ everest_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-calyx
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-everest
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
