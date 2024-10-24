CalyxOS/device_google_gs101

Add liboemcrypto.so to vendor linker config
This lets com.google.android.widevine.apex access it.

Change-Id: I4e9654fe6317b722f5fbf43b92abb367c18640ce

calyx_common.mk
@@ -15,3 +15,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # EUICC
 PRODUCT_PACKAGES += \
     EuiccSupportPixelOverlay
+
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    device/google/gs101/linker.config.json

slogar25/device_google_zumapro

calyx_common.mk
@@ -11,3 +11,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-calyx
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    device/google/zumapro/linker.config.json